cb37bd87bccc399bb3f3a71869651b15ca0cfa09
dkawadePromobi <dhanraj.kawade@promobitech.com>
2024-07-18 17:19:10 +0530

    Update Package.swift
    Signed-off-by: dkawadePromobi <dhanraj.kawade@promobitech.com>

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "WireGuardKit",
     platforms: [
-        .macOS(.v12),
-        .iOS(.v15)
+        .iOS(.v12)
     ],
     products: [
         .library(name: "WireGuardKit", targets: ["WireGuardKit"])
